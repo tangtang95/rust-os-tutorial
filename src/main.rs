3cc73de508146d6507c11ef202c0950cb4ccf4ca
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2023-01-05 11:57:06 +0100

    Add initial kernel implementation
    Features:
     - I/O: Writing to screen is implemented by writing on VGA buffer

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    
+    loop {}
+}
